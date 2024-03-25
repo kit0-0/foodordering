e75a31aa5f670e70512cb5d1f42fecd95623cc49
kiko <kt1767@gmail.com>
2024-03-26 03:39:50 +0530

    Add types.ts and ProductListItem component

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,50 @@
+import { StyleSheet, Image, Text, View } from 'react-native';
+
+import products from '../../assets/data/products';
+import Colors from '../constants/Colors';
+import { Product } from '../types';
+const product = products[0];
+type ProductListItemProps = {
+  product: Product;
+};
+
+
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{ uri: product.image || 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png' }}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+    </View>
+  );
+}
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 10,
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+    alignSelf: 'center',
+  },
+  title: {
+    fontWeight: '600',
+    fontSize: 18,
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+    marginTop: 'auto',
+  },
+});
