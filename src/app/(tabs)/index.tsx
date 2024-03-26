9c8fe4630bf64b1a8b5293713e0234ea5992dc74
kiko <kt1767@gmail.com>
2024-03-26 20:13:49 +0530

    Fix import paths in _layout.tsx and index.tsx

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,13 @@
-import {  View } from 'react-native';
+import { View } from "react-native";
 
-import products from '../../../assets/data/products';
-import ProductListItem from '../../components/ProductListItem';
-
-
-
+import products from '@assets/data/products';
+import ProductListItem from "../../components/ProductListItem";
 
 export default function MenuScreen() {
   return (
-    <View >
-     <ProductListItem product={products[0]} />
-<ProductListItem product={products[1]} />
+    <View>
+      <ProductListItem product={products[0]} />
+      <ProductListItem product={products[1]} />
     </View>
   );
 }
